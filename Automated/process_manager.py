a067ba83a1b1e3aa7aac99a03151f906fa8b7ff5
MattStew87 <matstewart.668@gmail.com>
2024-12-22 12:11:15 -0500

    clean up

--- Automated/process_manager.py
+++ Automated/process_manager.py
@@ -6,6 +6,7 @@ def run_updates():
     """
     Function to execute updates for all registered tables.
     """
+    print("run_updates has been called")
     registry = UpdateRegistry()
     registry.execute_updates()
 
@@ -16,7 +17,7 @@ def start_process_manager():
     # Schedule the update function to run every day at midnight
     schedule.every().day.at("00:00").do(run_updates)
 
-    print("Process Manager started. Updates will run daily at midnight.")
+    print("\nProcess Manager started. Updates will run daily at midnight.")
     
     # Keep the script running to check for scheduled tasks
     while True:
